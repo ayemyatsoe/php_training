8ad4e8f6a3cdcef8b75becef0c55ff07d15f3a00
AyeMyatMyatSoe <ayemyatmyatsoe75@gmail.com>
2022-02-21 11:35:52 +0630

    [master]

--- tutorials/tutorial_6/index.php
+++ tutorials/tutorial_6/index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Upload</title>
 </head>
+
 <body>
     <h3>File Store</h3>
     <form method="Post" enctype="multipart/form-data">
-        <input type="file" name="profile"><br><br><br>
+        <input type="file" name="profile" accept="image/x-png, image/gif, image/jpeg"><br><br><br>
         <input type="text" name="folder"><br><br><br>
         <input type="submit" name="btn" value="Save">
     </form>
@@ -35,4 +37,5 @@
         }
     ?>
 </body>
+
 </html>
